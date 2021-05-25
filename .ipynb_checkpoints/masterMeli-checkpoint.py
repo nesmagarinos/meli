cbcb019d76e0c15baf2bbef7600248cf4ad22d01
nesmagarinos <58562698+nesmagarinos@users.noreply.github.com>
2021-05-25 20:08:51 -0300

    .

--- .ipynb_checkpoints/masterMeli-checkpoint.py
+++ .ipynb_checkpoints/masterMeli-checkpoint.py
@@ -1,44 +1,63 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[69]:
+# In[1]:
 
 
 from datetime import datetime
 import requests
 import json
 import pandas as pd
-import seaborn as sns
+import streamlit as st
+import plotly.express as px
 
 
-# In[56]:
+# In[2]:
 
 
-url = "https://api.mercadolibre.com/sites/MLA/search?q=nintendo+switch&condition=new&limit=50"  # Endpoint a consultar
-response = requests.get(url)
+st.write("""
+# Titulo de prueba
+
+Script de MeLi
+""")
 
 
-# In[61]:
+# In[3]:
 
 
-data = json.loads(response.text)
-results=pd.DataFrame(data["results"])
+keywords_input = st.text_input("Búsqueda", "Ingrese una búsqueda")
 
 
-# In[71]:
+# In[4]:
 
 
-sns.histplot(results['price'])
+def chart(keywords_input):
+    keywords_input= keywords_input[0].upper()+keywords_input[1:].lower()
+    keywords = keywords_input.replace(" ","+")
+    url = "https://api.mercadolibre.com/sites/MLA/search?q="+keywords+"&condition=new&limit=50"  # Endpoint a consultar
+    response = requests.get(url)
+    data = json.loads(response.text)
+    results=pd.DataFrame(data["results"])
+
+    fig = px.histogram(results['price'],histnorm="percent",nbins=15)# y="tip", color="sex", marginal="rug",hover_data=df.columns
+    fig.layout.update(showlegend=True, 
+                      legend = {"title":{'text':""}},
+                      yaxis =  {"title": {"text": "Densidad"}},
+                      xaxis =  {"title": {"text": "Precio"}},
+                      title={'text': keywords_input,
+                        'x':0.5,
+                        'xanchor': 'center',
+                        'yanchor': 'top'}
+                     ) 
+    st.plotly_chart(fig)
 
 
-# In[72]:
+# In[5]:
 
 
-#get_ipython().system(' jupyter nbconvert --to script masterMeli.ipynb')
+if keywords_input!="Ingrese una búsqueda":
+    chart(keywords_input)
 
 
-# In[ ]:
-
-
-
+# In[6]:
 
