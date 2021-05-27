8999e5a71dc1e0fc9a35b384db3f1ec44091c162
nesmagarinos <58562698+nesmagarinos@users.noreply.github.com>
2021-05-27 14:07:19 -0300

    Update masterMeli.py

--- masterMeli.py
+++ masterMeli.py
@@ -14,7 +14,7 @@ import unidecode
 # In[2]:
 
 
-st.markdown("<img src="Meli_Logo.png" alt="MercadoLibre" class="center">")
+st.image("https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/v1456160088/dauw8qmiutpmqx4wvwlf.png",layout='centered')
 st.write("""
 ## Buscador de precios
 """)
